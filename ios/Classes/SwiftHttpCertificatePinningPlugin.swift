8887ee701cc3e7bfff1786af1225b3c61b3e1f68
JET <57088869+jethroverganio1997@users.noreply.github.com>
2024-09-12 20:49:44 +0800

    update: alamofire sessionmanager to sessios from version 5 onwards

--- ios/Classes/SwiftHttpCertificatePinningPlugin.swift
+++ ios/Classes/SwiftHttpCertificatePinningPlugin.swift
@@ -5,7 +5,7 @@ import Alamofire
 
 public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
 
-    let manager = Alamofire.SessionManager.default
+    let session = Alamofire.Session.default
     var fingerprints: Array<String>?
     var flutterResult: FlutterResult?
 
@@ -17,18 +17,18 @@ public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch (call.method) {
-            case "check":
-                if let _args = call.arguments as? Dictionary<String, AnyObject> {
-                    self.check(call: call, args: _args, flutterResult: result)
-                } else {
-                    result(
-                        FlutterError(
-                            code: "Invalid Arguments",
-                            message: "Please specify arguments",
-                            details: nil)
-                    )
-                }
-                break
+        case "check":
+            if let _args = call.arguments as? Dictionary<String, AnyObject> {
+                self.check(call: call, args: _args, flutterResult: result)
+            } else {
+                result(
+                    FlutterError(
+                        code: "Invalid Arguments",
+                        message: "Please specify arguments",
+                        details: nil)
+                )
+            }
+            break
         default:
             result(FlutterMethodNotImplemented)
         }
@@ -38,7 +38,7 @@ public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
         call: FlutterMethodCall,
         args: Dictionary<String, AnyObject>,
         flutterResult: @escaping FlutterResult
-    ){
+    ) {
         guard let urlString = args["url"] as? String,
               let headers = args["headers"] as? Dictionary<String, String>,
               let fingerprints = args["fingerprints"] as? Array<String>,
@@ -61,18 +61,20 @@ public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
             timeout = timeoutArg
         }
         
-        let manager = Alamofire.SessionManager(
-            configuration: URLSessionConfiguration.default
-        )
+        // Configure the session with custom settings
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = TimeInterval(timeout)
         
-        var resultDispatched = false;
+        // Initialize the session with the configuration
+        let session = Alamofire.Session(configuration: configuration)
         
-        manager.session.configuration.timeoutIntervalForRequest = TimeInterval(timeout)
+        var resultDispatched = false
         
-        manager.request(urlString, method: .get, parameters: headers).validate().responseJSON() { response in
+        // Making the request with the session
+        session.request(urlString, method: .get, headers: HTTPHeaders(headers)).validate().responseJSON { response in
             switch response.result {
-                case .success:
-                    break
+            case .success:
+                break
             case .failure(let error):
                 if (!resultDispatched) {
                     flutterResult(
@@ -82,17 +84,15 @@ public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
                             details: nil
                         )
                     )
-               }
-                   
+                }
                 break
             }
-            
-            // To retain
-            let _ = manager
         }
 
-        manager.delegate.sessionDidReceiveChallenge = { session, challenge in
-            guard let serverTrust = challenge.protectionSpace.serverTrust, let certificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else {
+        // Set up certificate pinning
+        session.session.delegate.taskDidReceiveChallenge = { session, task, challenge in
+            guard let serverTrust = challenge.protectionSpace.serverTrust,
+                  let certificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else {
                 flutterResult(
                     FlutterError(
                         code: "ERROR CERT",
@@ -100,35 +100,30 @@ public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
                         details: nil
                     )
                 )
-                
                 return (.cancelAuthenticationChallenge, nil)
             }
 
             // Set SSL policies for domain name check
-            let policies: [SecPolicy] = [SecPolicyCreateSSL(true, (challenge.protectionSpace.host as CFString))]
+            let policies: [SecPolicy] = [SecPolicyCreateSSL(true, challenge.protectionSpace.host as CFString)]
             SecTrustSetPolicies(serverTrust, policies as CFTypeRef)
 
             // Evaluate server certificate
             var result: SecTrustResultType = .invalid
             SecTrustEvaluate(serverTrust, &result)
-            let isServerTrusted: Bool = (result == .unspecified || result == .proceed)
+            let isServerTrusted = (result == .unspecified || result == .proceed)
 
             let serverCertData = SecCertificateCopyData(certificate) as Data
             var serverCertSha = serverCertData.sha256().toHexString()
 
-            if(type == "SHA1"){
+            if (type == "SHA1") {
                 serverCertSha = serverCertData.sha1().toHexString()
             }
 
             var isSecure = false
             if var fp = self.fingerprints {
-                fp = fp.compactMap { (val) -> String? in
-                    val.replacingOccurrences(of: " ", with: "")
-            }
+                fp = fp.compactMap { $0.replacingOccurrences(of: " ", with: "") }
 
-                isSecure = fp.contains(where: { (value) -> Bool in
-                    value.caseInsensitiveCompare(serverCertSha) == .orderedSame
-                })
+                isSecure = fp.contains { $0.caseInsensitiveCompare(serverCertSha) == .orderedSame }
             }
 
             if isServerTrusted && isSecure {
